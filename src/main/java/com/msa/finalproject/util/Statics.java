83064fefaa09d2535cb9346e8d4f5340a3c93c53
happy5happy5 <zombil8731@gmail.com>
2023-09-24 16:02:59 +0900

    fix style.css

--- src/main/java/com/msa/finalproject/util/Statics.java
+++ src/main/java/com/msa/finalproject/util/Statics.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum Statics {
-    PAGE_SIZE(3),
+    PAGE_SIZE(5),
     BLOCK_SIZE(5),
     DUMMY_DATA_SIZE(1000),
     EXPIRATION_TIME(1000 * 60 * 60 * 24 * 7); // 7 days
